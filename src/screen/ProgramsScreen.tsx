6d10ffff2e21e15e1728114d5c2586e7d3e1d4d7
Jean-Baptiste Dusseaut <jb@benderlabs.io>
2021-05-10 11:02:12 +0200

    fix: ProgramsScreen.tsx was missing a closing tag.

--- src/screen/ProgramsScreen.tsx
+++ src/screen/ProgramsScreen.tsx
@@ -6,51 +6,51 @@ import ProgramList from '../features/program/ProgramList';
 import { useHistory } from 'react-router';
 import { opPage } from '../routes';
 
-const useStyles = makeStyles((theme) => createStyles({
-  subtitle: {
-    color: '#000000',
-    textAlign: 'center'
-  },
-  test: {
-    borderRadius: '0 0 10px 10px',
-    padding: '30px',
-    backgroundColor: '#e5e5e5'
-  },
-  title: {
-    color: 'white',
-    borderBottom: '3px solid #ffd000',
-    textAlign: 'center',
-    paddingBottom: '15px'
-  },
-  titleCenter: {
-    justifyItems: 'center'
-  }
-}));
-
+const useStyles = makeStyles(() =>
+  createStyles({
+    subtitle: {
+      color: '#000000',
+      textAlign: 'center',
+    },
+    test: {
+      borderRadius: '0 0 10px 10px',
+      padding: '30px',
+      backgroundColor: '#e5e5e5',
+    },
+    title: {
+      color: 'white',
+      borderBottom: '3px solid #ffd000',
+      textAlign: 'center',
+      paddingBottom: '15px',
+    },
+    titleCenter: {
+      justifyItems: 'center',
+    },
+  })
+);
 
 export default function ProgramsScreen() {
   const classes = useStyles();
   const history = useHistory();
   const { programs } = useConfig();
   return (
-    <Container  maxWidth={'md'}>
-
-      <Box className={classes.titleCenter} my={2} >
-        <Typography component={'h1'} variant={'h2'} className={classes.title}>Liquidity Mining Programs</Typography>
-
-  
+    <Container maxWidth={'md'}>
+      <Box className={classes.titleCenter} my={2}>
+        <Typography component={'h1'} variant={'h2'} className={classes.title}>
+          Liquidity Mining Programs
+        </Typography>
       </Box>
       <Box className={classes.test}>
-        <Typography variant={'subtitle1'} className={classes.subtitle}>Select an option to stake, unstake or claim your
-          rewards.</Typography>
-      <ProgramList
-
-        programs={programs}
-        onProgramSelect={(t) => {
-          history.push(opPage(t));
-        }}
-      />
+        <Typography variant={'subtitle1'} className={classes.subtitle}>
+          Select an option to stake, unstake or claim your rewards.
+        </Typography>
+        <ProgramList
+          programs={programs}
+          onProgramSelect={(t) => {
+            history.push(opPage(t));
+          }}
+        />
+      </Box>
     </Container>
   );
-
 }
